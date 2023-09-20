2033734842ba2f73ab1e52b1c17d3f004ee29dea
Mark-Kinyua <markinyua@gmail.com>
2023-09-20 13:37:01 +0300

    Implemented React Routers in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import { Container } from "react-bootstrap";
+
+import { Outlet } from 'react-router-dom';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
+
+
+
 
 const App = () => {
   return (
@@ -9,7 +15,7 @@ const App = () => {
       < Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to Sivma Shop</h1>
+          <Outlet />
         </Container>
       </main>
       < Footer />
---
+++ frontend/src/products.js
@@ -0,0 +1,41 @@
+const products = [
+    {
+        _id: '1',
+        name: 'Airpods',
+        image: '/images/1.jpg',
+        description: 'fbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb',
+        brand: 'Apple',
+        category: 'Electronics',
+        price: 79.99,
+        countInStock: 0,
+        rating: 4.0,
+        numReviews: 12,
+    },
+    {
+        _id: '2',
+        name: 'Headphones',
+        image: '/images/2.jpg',
+        description: 'fbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb',
+        brand: 'JBL',
+        category: 'Electronics',
+        price: 49.99,
+        countInStock: 3,
+        rating: 3.0,
+        numReviews: 18,
+    },
+    {
+        _id: '3',
+        name: 'Super Max 3000',
+        image: '/images/2.jpg',
+        description: 'fbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb',
+        brand: 'Nike',
+        category: 'Clothing',
+        price: 29.99,
+        countInStock: 4,
+        rating: 4.5,
+        numReviews: 19,
+    },
+
+]
+
+export default products
